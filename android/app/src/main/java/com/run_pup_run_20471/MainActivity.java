608edfa997d4d81236997abb2f905092cffde85c
Crowdbotics <team@crowdbotics.com>
2020-09-20 03:30:09 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/run_pup_run_20471/MainActivity.java
@@ -0,0 +1,15 @@
+package com.run_pup_run_20471;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "run_pup_run_20471";
+    }
+}
